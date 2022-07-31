5d3a73357de67b480e64a5f2dd90ea6f6430580a
alvalenda <banysan@msn.com>
2022-07-31 16:04:49 -0300

    +2 flavours

--- src/services/paletas.service.js
+++ src/services/paletas.service.js
@@ -11,7 +11,7 @@ const paletas = [
     id: 2,
     sabor: 'Banana com Nutella',
     descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, distinctio!',
     foto: 'assets/images/banana-com-nutella.png',
     preco: 10.0,
   },
@@ -19,9 +19,25 @@ const paletas = [
     id: 3,
     sabor: 'Chocolate Belga',
     descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, totam!',
     foto: 'assets/images/chocolate-belga.png',
     preco: 7.0,
+  },
+  {
+    id: 4,
+    sabor: 'Napotilano',
+    descricao:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, nisi?',
+    foto: 'assets/images/napolitano.png',
+    preco: 8.5,
+  },
+  {
+    id: 5,
+    sabor: 'Limão',
+    descricao:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nihil.',
+    foto: 'assets/images/limao.png',
+    preco: 6.0,
   },
 ];
 
