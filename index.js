8b50e616f3b6dbba2b804fe10eb898a3773e5b38
alvalenda <banysan@msn.com>
2022-07-31 22:56:20 -0300

    +mongoose +mongodb setted

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/paletas.route');
+const connectToDatabase = require('./src/database/database');
 
 const port = 3000;
 const app = express();
 
 /* Novo trecho */
+connectToDatabase();
+/* Novo Trecho */
+
 app.use(express.json());
 app.use(cors());
 app.use('/paletas', routes);
-/* Novo Trecho */
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
